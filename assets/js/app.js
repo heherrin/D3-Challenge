aeab6ab1e1be9f88b8ec788bc01e8dbed6d7a978
Heath Herrington <heathherrington@comcast.net>
2021-03-02 02:11:42 -0600

    svg setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,18 @@ var chartMargin = {
   left:60
 };
 
+// Set the dimensions for chart
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select scatter in html & append svg & dimensions
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
