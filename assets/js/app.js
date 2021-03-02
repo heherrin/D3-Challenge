a89e317ee545f88bdee5eed4beaff60856a9124f
Heath Herrington <heathherrington@comcast.net>
2021-03-02 00:55:18 -0600

    add margins and svg height/width

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-// @TODO: YOUR CODE HERE!
+// set svg and dimensions of chart
+
+//Set variable for height and width for calculation
+var svgWidth = 900;
+var svgHeight = 500;
+
+//set chart margins
+var chartMargin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left:60
+};
+
