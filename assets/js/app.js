6d42356688c3962ca88fb4883ef85ee0d002ce29
Heath Herrington <heathherrington@comcast.net>
2021-03-02 03:06:07 -0600

    create scaling & axis

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,10 @@ var svgHeight = 500;
 
 //set chart margins
 var chartMargin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left:60
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 60
 };
 
 // Set the dimensions for chart
@@ -18,19 +18,41 @@ var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
 // Select scatter in html & append svg & dimensions
 var svg = d3.select("#scatter")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
 // Append a group to the SVG area and shift ('translate') it to the right and down to adhere
 // to the margins set in the "chartMargin" object.
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-  // Load data from journalism data csv
-  d3.csv("assets/data/data.csv").then(function(journalismData) {
+// Load data from journalism data csv
+d3.csv("assets/data/data.csv").then(function (journalismData) {
 
-    // Print the tvData
+    // Print the csv data
     console.log(journalismData);
-  })
-  
+
+    // Cast the hours value to a number for each piece of tvData
+    journalismData.forEach(function (chartData) {
+        chartData.income = +chartData.income;
+        chartData.smokes = +chartData.smokes;
+
+        //console log data
+        console.log(chartData.income);
+        console.log(chartData.smokes);
+
+        //creating scaling
+        var xScale = d3.scaleLinear()
+            .domain([0, d3.max(journalismData, d => d.income)])
+            .range([0, chartWidth]);
+
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(journalismData, d => d.smokes)])
+            .range([chartHeight, 0])
+
+
+
+    });
+})
+
